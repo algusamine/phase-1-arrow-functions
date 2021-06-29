e2c16d2a02549af94d1d31603a523e75fe6abab6
Amine <algusamine@gmail.com>
2021-06-29 00:19:12 -0400

    commit tree 9f57553a162a62fd7adecf3b47f79a23c8b266d7

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(num1, num2){
+    return 2000/100;
+}
+divide();
+
+const square =(num)=> {
+    return num * num;
+}
+square();
+
+const add =(num1, num2)=>{
+    return num1 + num2;
+}
+add()
